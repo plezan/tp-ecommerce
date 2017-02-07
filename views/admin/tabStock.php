a2caae5de409b549c874aed0aa7018c9db5da940
plezan <1pierrelezan@gmail.com>
2017-02-07 21:39:32 +0100

    update

--- views/admin/tabStock.php
+++ views/admin/tabStock.php
@@ -1,1 +1,16 @@
-<h1>tabStock</h1>
+<table>
+<tr>
+  	<th>ID</th>
+    <th>Name</th>
+    <th>Price</th> 
+    <th>Action</th>
+ </tr>
+<?php foreach($listProduit as $produit){ ?>
+  <tr>
+  	<td id=<?php echo $produit['art_id']; ?>><?php echo $produit['art_id']; ?></td>
+    <td><?php echo $produit['art_name']; ?></td>
+    <td><?php echo $produit['art_price'] ?></td> 
+    <td>nothing</td>
+  </tr>
+<?php } ?>
+</table>
--- views/home/homepage.php
+++ views/home/homepage.php
@@ -3,6 +3,6 @@
 		<h2><?php echo $produit['art_name']; ?></h2>
 		<p><?php echo $produit['art_description']; ?></p>
 		<p>Prix : <i><?php echo $produit['art_price'] ?> €</i></p>
-		<?php if($_SESSION['login'] == 2){ echo "<a>Modifier</a>";} ?>
+		<?php if($_SESSION['login'] == 2){ echo '<a href="account.php?tab=2#'.$produit['art_id'].'">Modifier</a>';} ?>
 	</div>
 <?php } ?>
